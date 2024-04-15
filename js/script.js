c07010d8c9da567ab2107ab1a38a191c060de092
李文豪 <1024936291@qq.com>
2024-04-15 15:55:10 +0800

    second

--- js/script.js
+++ js/script.js
@@ -78,7 +78,7 @@ const mainStage = new Stage("main-canvas");
 const stages = [trailsStage, mainStage];
 
 //随机文字烟花内容
-const randomWords = ["生日快乐", "二战成硕","小刘颖小宝贝"];
+const randomWords = ["生日快乐", "心想事成","小刘颖小宝贝","美丽动人","聪明可爱","机智过人","小百灵鸟"];
 const wordDotsMap = {};
 randomWords.forEach((word) => {
 	wordDotsMap[word] = MyMath.literalLattice(word, 3, "Gabriola,华文琥珀", "90px");
